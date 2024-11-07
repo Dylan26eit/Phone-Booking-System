7e1137ba4821ed8863ed2527c6ed73d599fdbc60
Dylan <anderd26@student.eit.ac.nz>
2024-11-07 13:43:39 +1300

    Final maybe

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./my-components/Header"
 import Home from "./my-components/Home"
 import Footer from "./my-components/Footer"
 import AdvancedJS from './my-components/AdvancedJS';//import AdvancedJS Component
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import FAQ from './my-components/FAQ';//import FAQ Component
 import Invoice from "./my-components/Invoice";
 import { DndProvider } from 'react-dnd';
